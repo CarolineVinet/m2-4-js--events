64040c9f7155d2c7e1a7f11d61659fc6737d582b
Caroline Vinet <carovinet@gmail.com>
2020-06-18 15:11:03 -0400

    exercise2

--- workshop/exercise-1.1/app.js
+++ workshop/exercise-1.1/app.js
@@ -1,3 +1,23 @@
-// Exercise 1.1
-// ------------
-console.log('exercise 1.1');
+const message = document.querySelector("body");
+const winMessage = document.querySelector("p");
+const quicker = document.createElement("h2");
+let winner = false;
+let loser = false;
+
+quicker.innerText = "Be a quicker clicker!";
+
+message.appendChild(quicker);
+
+addEventListener("click", function () {
+  if (loser === false) {
+    winMessage.innerText = "You Win!";
+    winner = true;
+  }
+});
+
+setTimeout(function () {
+  if (winner === false) {
+    winMessage.innerText = " You Lose!";
+    loser = true;
+  }
+}, 3000);
