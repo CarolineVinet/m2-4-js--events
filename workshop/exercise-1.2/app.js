cbc26eca0e6895e481f7564bd84baf3146b838c9
Caroline Vinet <carovinet@gmail.com>
2020-06-18 17:14:14 -0400

    exercise1.2

--- workshop/exercise-1.2/app.js
+++ workshop/exercise-1.2/app.js
@@ -1,3 +1,23 @@
-// Exercise 1.2
-// ------------
-console.log('exercise 1.2');
+const game = document.querySelector("body");
+const result = document.querySelector("#result");
+const time = document.querySelector("#time");
+
+let winner = false;
+let loser = false;
+let randomNum = Math.floor(Math.random() * 5000);
+let numDisplay = Math.floor(randomNum / 1000);
+time.innerText = numDisplay;
+
+addEventListener("click", function () {
+  if (loser === false) {
+    result.innerText = " You Win!";
+    winner = true;
+  }
+});
+
+setTimeout(function () {
+  if (winner === false) {
+    result.innerText = "You Lose!";
+    loser = true;
+  }
+}, randomNum);
